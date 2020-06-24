b862569e1d467d1385b826c935f3c6c8af0e662e
Rohit Sharma <rsedwardian@gmail.com>
2020-06-24 23:24:44 +0530

    updating timestamps

--- repoimpl/mysqldb.go
+++ repoimpl/mysqldb.go
@@ -1,8 +1,22 @@
 package repoimpl
 
 import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type MySqlClient struct {
-    *
+    *sql.DB
 }
+
+func Init() (*MySqlClient) {
+    db, err := sql.Open("mysql", "user:password@/dbname")
+	if err != nil {
+        // do proper error handling
+		panic(err.Error())  
+	}
+	defer db.Close()
+}
+
+func Get(query string, 
