a65871b561e7081352b6a5765cc0cdd0d8781747
Rohit Sharma <rsedwardian@gmail.com>
2020-06-24 23:38:59 +0530

    updating timestamps

--- repoimpl/mysqldb.go
+++ repoimpl/mysqldb.go
@@ -7,16 +7,52 @@ import (
 )
 
 type MySqlClient struct {
-    *sql.DB
+    Conn *sql.DB
 }
 
-func Init() (*MySqlClient) {
-    db, err := sql.Open("mysql", "user:password@/dbname")
+const (
+    getUserByEmail          string = `select * from Users where email = ?`
+)
+
+func Init(cfg config.DBConfig) (*MySqlClient, error) {
+    connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", cfg.User, cfg.Pass, cfg.Host, cfg.Port, cfg.Name)
+	val := url.Values{}
+	val.Add("charset", "utf8")
+	val.Add("parseTime", "True")
+	val.Add("loc", "Local")
+	dsn := fmt.Sprintf("%s?%s", connection, val.Encode())
+	dbConn, err := sql.Open(`mysql`, dsn)
 	if err != nil {
-        // do proper error handling
-		panic(err.Error())  
+		return nil, err
 	}
-	defer db.Close()
+	err = dbConn.Ping()
+	if err != nil {
+		return nil, err
+	}
+	dbConn.SetMaxOpenConns(cfg.MaxConnections)
+	return dbConn, nil
 }
 
-func Get(query string, 
+func (sql *MySqlClient) GetUserByEmail(ctx context.Context, email string) ([]domains.UserDetails, error) {
+    userRows, err := sql.Conn.QueryContext(ctx, getUserByEmail, email)
+    if err != nil {
+        fmt.Printf("Error while fetching user ", err)
+        return nil
+    }
+    if userRows != nil {
+        defer userRows.Close()
+    }
+
+    var userDetailRows []domains.UserDetails
+    for userRows.Next() {
+        user := domains.UserDetails{}
+        err := StructScan(userRows, &user)
+
+        if err != nil {
+            fmt.Printf("cannot understand this struct")
+            return nil
+        }
+        userDetailRows = append(userDetailRows, user) 
+    }
+    return userDetailRows, nil
+}
